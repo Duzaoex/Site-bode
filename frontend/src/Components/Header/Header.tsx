c4ddd11964384ec45859f8c067a8535102455617
Eduardo Neris Carneiro <92535485+Duzaoex@users.noreply.github.com>
2024-09-08 02:51:35 +0000

    inicio

---
+++ frontend/src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header>
+            <div>
+            </div>
+            <h1>nome do site</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Bem-vindo ao Meu Site</h2>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
