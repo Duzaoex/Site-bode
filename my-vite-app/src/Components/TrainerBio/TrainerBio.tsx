828af08440b3fc23f75b40481c735495b05b0b07
Duzaoex <eduardoneriscarneiro@gmail.com>
2024-09-16 18:29:58 -0300

    1

--- my-vite-app/src/Components/TrainerBio/TrainerBio.tsx
+++ my-vite-app/src/Components/TrainerBio/TrainerBio.tsx
@@ -3,9 +3,12 @@ import styles from './TrainerBio.module.css';
 function TrainerBio() {
   return (
     <section id="about" className={styles.bio}>
-      <h2>Quem é André Castanho?</h2>
+      <h2 className={styles.h2}>Quem é André Castanho?</h2>
       <img src="src/Helpers/Perfilbode.jpeg" alt="André Castanho" className={styles.photo} />
-      <p>André é um profissional com mais de 10 anos de experiência na área de musculação...</p>
+      <p className={styles.p}>" André Castanho é especialista em musculação, com mais de 10 anos de experiência. 
+        Pós-graduado em Fisiologia do Exercício e especializado em Prescrição de Treinamento, 
+        ele oferece um conhecimento profundo sobre o impacto do exercício no corpo. Atualmente, 
+        cursa Nutrição, ampliando sua abordagem integrada para o bem-estar de seus clientes. "</p>
     </section>
   );
 }
