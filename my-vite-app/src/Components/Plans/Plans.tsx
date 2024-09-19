775a25770269d735cf0dd39faade850f6b353c5a
Duzaoex <eduardoneriscarneiro@gmail.com>
2024-09-19 13:44:44 -0300

    1

--- my-vite-app/src/Components/Plans/Plans.tsx
+++ my-vite-app/src/Components/Plans/Plans.tsx
@@ -7,7 +7,7 @@ function Plans() {
       <div className={styles.plan}>
         <h3 className={styles.h3}>Acompanhamento Trimestral (03 Meses)</h3>
         <div className={styles.features}>
-          <h4>Funcionalidades do App:</h4>
+          <h4>Vantagens desse Plano:</h4>
           <ul>
             <li>✔ Exercícios no app exclusivo para facilitar a visualização e execução durante o treino.</li>
             <li>✔ Atualização mensal de fotos dentro do app para acompanhar seu progresso.</li>
@@ -32,7 +32,7 @@ function Plans() {
       <div className={styles.plan}>
         <h3 className={styles.h3}>Acompanhamento Semestral (06 Meses)</h3>
         <div className={styles.features}>
-          <h4>Funcionalidades do App:</h4>
+          <h4>Vantagens desse Plano:</h4>
           <ul>
             <li>✔ Exercícios no app exclusivo para facilitar a visualização e execução durante o treino.</li>
             <li>✔ Atualização mensal de fotos dentro do app para acompanhar seu progresso.</li>
@@ -57,7 +57,7 @@ function Plans() {
       <div className={styles.plan}>
         <h3 className={styles.h3}>Acompanhamento Anual (12 Meses)</h3>
         <div className={styles.features}>
-          <h4>Funcionalidades do App:</h4>
+          <h4>Vantagens desse Plano:</h4>
           <ul>
             <li>✔ Exercícios no app exclusivo para facilitar a visualização e execução durante o treino.</li>
             <li>✔ Atualização mensal de fotos dentro do app para acompanhar seu progresso.</li>
