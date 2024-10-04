46ff3f52d6006b0e173c2cf26bbde4e48edeeb66
Duzaoex <eduardoneriscarneiro@gmail.com>
2024-10-04 13:53:28 -0300

    1

--- my-vite-app/src/Components/Video/Video.tsx
+++ my-vite-app/src/Components/Video/Video.tsx
@@ -9,7 +9,7 @@ const VideoSection: React.FC = () => {
       <h2 className={style.h2}>Aplicativo</h2>
       <div className={style.videoContainer}>
         <iframe
-          src="public/Helpers/WhatsApp Video 2024-09-25 at 05.37.31.mp4"
+          src="/Helpers/WhatsApp Video 2024-09-25 at 05.37.31.mp4"
           title="Embedded Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
